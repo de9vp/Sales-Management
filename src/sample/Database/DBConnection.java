dcac5ea965afbc62bc4d1a582520314d5824d7ab
Phuc <phxc29@gmail.com>
2021-05-17 13:07:00 +0700

    git status

--- src/sample/Database/DBConnection.java
+++ src/sample/Database/DBConnection.java
@@ -5,16 +5,8 @@ import javafx.fxml.FXML;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
-import java.util.logging.Logger;
 
 public class DBConnection {
-    //Connection connect = null;
-    //public static DBConnection handler = null;
-
-
-
-
-
     public static Connection DBConn() {
         try {
             String url = "jdbc:sqlserver://DESKTOP-QEN4LJI\\SQLEXPRESS;database=DBJavaFx";
@@ -30,13 +22,4 @@ public class DBConnection {
             return null;
         }
     }
-
-    //get Instance
-    /*public static DBConnection getInstance() {
-        if(handler == null) {
-            handler = new DBConnection();
-        }
-        return handler;
-    }*/
-
 }
